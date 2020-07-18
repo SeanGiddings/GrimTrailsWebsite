5e40ccc9a734aa4d418fc1e94fea04a49e48835e
seangiddings <seandgiddings@gmail.com>
2020-07-17 22:27:04 -0400

    added faq page to site, functioning in WP

---
+++ faq.php
@@ -0,0 +1,122 @@
+<?php /* Template Name: FAQ*/ ?>
+
+<?php get_header(); ?>
+
+  <div class="body__wrapper">
+
+      <!--Buy Tickets-->
+      <div class="tickets-button">
+      <a href="/tickets">Buy Tickets Online</a>
+    </div>
+
+      <!--Tertpage Title-->
+      <div class="tertpage__container">
+        <div class="tertpage__title">
+          <h1>
+            Frequently Asked Questions
+          </h1>
+        </div>
+
+        <div class="tertpage__text">
+          <p>
+            If you've read the F.A.Q. below, but still have questions, or want
+            to contact us about something else, please send us an email and
+            we'll help you with anything you need.
+          </p>
+        </div>
+
+        <!--Map-->
+        <div class="faq__container">
+          <h2 class="faq__question">
+            What is your attraction?
+          </h2>
+          <p class="faq__answer">
+            Grim Trails is an award-winning fairy-tale and folklore themed
+            haunted trail with scenes woven throughout small buildings and
+            woods. Don’t believe us? Check out our preview video!
+          </p>
+
+          <h2 class="faq__question">
+            What age groups are appropriate?
+          </h2>
+          <p class="faq__answer">
+            While we believe Halloween is for everyone, we do not recommend our
+            haunted trail for children under 10 due to the fright factor.
+            Children under the age of 10 will receive the same terrifying show
+            as any other patron for the same price.
+          </p>
+
+          <h2 class="faq__question">
+            Will the monsters touch me?
+          </h2>
+          <p class="faq__answer">
+            Our haunted house has a no-touch policy- our actors and staff will
+            not touch you, and customers are not allowed to touch us. Due to
+            low-light conditions and narrow passages, our monsters may bump into
+            you. Our creatures will not push, grab or maul you.
+          </p>
+
+          <h2 class="faq__question">
+            Does your haunted house have security?
+          </h2>
+          <p class="faq__answer">
+            Yes! We value the safety of our staff and guests. We have LMPD
+            officers present at all times when the attraction is open.
+          </p>
+
+          <h2 class="faq__question">
+            Is it different from last year?
+          </h2>
+          <p class="faq__answer">
+            During the ten months of the year that we are closed, we endlessly
+            renovate and add new scenes designed to scare and excite. Follow us
+            on Facebook and Instagram for sneak peeks of everything we’re
+            dishing up this year.
+          </p>
+
+          <h2 class="faq__question">
+            What form of payment do you accept
+          </h2>
+          <p class="faq__answer">
+            We accept cash, Visa, MasterCard and Discover. Paypal as well!
+          </p>
+
+          <h2 class="faq__question">
+            Is it really Scary?
+          </h2>
+          <p class="faq__answer">
+            We certainly hope so!
+          </p>
+
+          <h2 class="faq__question">
+            If I get too scared and have to leave through an emergency exit, do
+            I get a refund?
+          </h2>
+          <p class="faq__answer">
+            No.
+          </p>
+
+          <h2 class="faq__question">
+            Do you use strobe lights?
+          </h2>
+          <p class="faq__answer">
+            Yes. We use strobe lights, fog, snow and laser machines.
+          </p>
+        </div>
+        <div class="disclaimer__container">
+          <h2 class="disclaimer__title">DISCLAIMER</h2>
+          <p class="disclaimer__text">Haunted Yard, LLC presents Grim Trails Haunted Attractions reserves the right to refuse a ride to any
+            patron at any time, for any reason. Your ticket is a revocable license and can be taken with admission
+            refused. All patrons will voluntarily assume all risks and dangers associated with the attraction and
+            agrees to release the operator(s), affiliates, officers, directors, employees, and landlord from any liability,
+            harm, injury or death, cost or expense whatsoever that may arise directly or indirectly from attending
+            this attraction. APPROPRIATE ATTIRE IS REQUIRED. You will experience audio & visual disturbances,
+            low visibility, strobe lights, fog, and damp conditions, encounter uneven walking surfaces in a physically
+            demanding environment. While every effort is made to ensure your safety, you should not participate
+            if you are: pregnant; suffer from claustrophobia; are prone to seizures; have heart, back, or respiratory
+            problems.</p>
+        </div>
+      </div>
+</div>
+
+      <?php get_footer(); ?>
