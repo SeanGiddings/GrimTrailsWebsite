c70ad3fe088c25194083d0d2c9983eed578378a1
seangiddings <seandgiddings@gmail.com>
2020-07-21 18:21:38 -0400

    added gallery page to wordpress, without any style

---
+++ gallery.php
@@ -0,0 +1,25 @@
+<?php /* Template Name: Gallery*/ ?>
+
+<?php get_header(); ?>
+
+  <div class="body__wrapper">
+
+     
+      <!--Haunt Description-->
+      <div class="tertpage__container">
+        <div class="tertpage__title">
+          <h1>
+            Gallery
+          </h1>
+        </div>
+
+        <div class="tertpage__text">
+
+        <p><br>Still have questions?  Please refer to our F.A.Q.</p>
+        </div>
+            
+
+      </div>
+</div>
+
+      <?php get_footer(); ?>
