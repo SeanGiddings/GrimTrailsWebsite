32313825d9481aca116e5533107f31584403263c
seangiddings <seandgiddings@gmail.com>
2020-08-07 13:37:31 -0400

    created child theme

---
+++ GrimTrailsWebsite-child/functions.php
@@ -0,0 +1,25 @@
+<?php
+function myprefix_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'myprefix_theme_enqueue_styles' ); ?>
+
+<?php
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Home right sidebar',
+		'id'            => 'home_right_1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+?>
