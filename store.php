eb38141c3fb4a05bfac038f4b69d4db6ce9683be
seangiddings <seandgiddings@gmail.com>
2020-08-26 16:47:14 -0400

    added store page and demo product cards

---
+++ store.php
@@ -0,0 +1,44 @@
+<?php /* Template Name: Store*/ ?>
+
+<?php get_header(); ?>
+
+<div class="body__wrapper">
+
+
+    <div class="container">
+        <div class="row">
+        <div class="card .col-sm-4">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ProductDemo.jpg" alt="Denim Jeans" style="width:100%">
+                <h1 class="product__title">Tailored Jeans</h1>
+                <p class="product__price">$19.99</p>
+                <p class="product__description">Some text about the jeans..</p>
+                <p><button>Add to Cart</button></p>
+            </div>
+            <div class="card .col-sm-4">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ProductDemo.jpg" alt="Denim Jeans" style="width:100%">
+                <h1 class="product__title">Tailored Jeans</h1>
+                <p class="product__price">$19.99</p>
+                <p class="product__description">Some text about the jeans..</p>
+                <p><button>Add to Cart</button></p>
+            </div>
+            <div class="card .col-sm-4">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ProductDemo.jpg" alt="Denim Jeans" style="width:100%">
+                <h1 class="product__title">Tailored Jeans</h1>
+                <p class="product__price">$19.99</p>
+                <p class="product__description">Some text about the jeans..</p>
+                <p><button>Add to Cart</button></p>
+            </div>
+            <div class="card .col-sm-4">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ProductDemo.jpg" alt="Denim Jeans" style="width:100%">
+                <h1 class="product__title">Tailored Jeans</h1>
+                <p class="product__price">$19.99</p>
+                <p class="product__description">Some text about the jeans..</p>
+                <p><button>Add to Cart</button></p>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+<?php get_footer(); ?>
