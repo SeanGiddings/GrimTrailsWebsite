57640bd81e871030eec37ec23955d1ac94b62c35
seangiddings <seandgiddings@gmail.com>
2020-08-27 22:35:54 -0400

    added demo cards and products

--- store.php
+++ store.php
@@ -7,28 +7,28 @@
 
     <div class="container">
         <div class="row">
-        <div class="card .col-sm-4">
+        <div class="card col-lg-4">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ProductDemo.jpg" alt="Denim Jeans" style="width:100%">
                 <h1 class="product__title">Tailored Jeans</h1>
                 <p class="product__price">$19.99</p>
                 <p class="product__description">Some text about the jeans..</p>
                 <p><button>Add to Cart</button></p>
             </div>
-            <div class="card .col-sm-4">
+            <div class="card col-lg-4">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ProductDemo.jpg" alt="Denim Jeans" style="width:100%">
                 <h1 class="product__title">Tailored Jeans</h1>
                 <p class="product__price">$19.99</p>
                 <p class="product__description">Some text about the jeans..</p>
                 <p><button>Add to Cart</button></p>
             </div>
-            <div class="card .col-sm-4">
+            <div class="card col-lg-4">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ProductDemo.jpg" alt="Denim Jeans" style="width:100%">
                 <h1 class="product__title">Tailored Jeans</h1>
                 <p class="product__price">$19.99</p>
                 <p class="product__description">Some text about the jeans..</p>
                 <p><button>Add to Cart</button></p>
             </div>
-            <div class="card .col-sm-4">
+            <div class="card col-lg-4">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ProductDemo.jpg" alt="Denim Jeans" style="width:100%">
                 <h1 class="product__title">Tailored Jeans</h1>
                 <p class="product__price">$19.99</p>
