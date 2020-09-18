49e56a3d11e55ce2710998b0654263ef82847263
seangiddings <seandgiddings@gmail.com>
2020-09-17 20:09:13 -0400

    added google analytics tag to head of site

--- header.php
+++ header.php
@@ -12,6 +12,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
   <link href="https://fonts.googleapis.com/css2?family=Federo&display=swap" rel="stylesheet">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-177636836-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-177636836-1');
+</script>
   <?php wp_head(); ?>
 </head>
 
