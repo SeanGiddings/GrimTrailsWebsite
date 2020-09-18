4f37877f822f2f8e0688c40f012c303fc3c156e2
seangiddings <seandgiddings@gmail.com>
2020-09-17 20:04:55 -0400

    added covid bullet points to covid page

--- covid.php
+++ covid.php
@@ -12,19 +12,40 @@
       <!--Tertpage Title-->
       <div class="tertpage__container">
         <div class="tertpage__title">
-          <h1>
+
+        <img id="covid" class="covid__title img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/images/covidtitle.png" alt="COVID RESPONSE title" aria-label="COVID response title image" />
+
+          <!--<h1>
             Covid-19 Information
-          </h1>
+          </h1>-->
         </div>
 
-        <div class="tertpage__text">
+        <!--<div class="tertpage__text">
           <p>
             Please see image below. For any additional questions, please contact us and we'll help you with anything you need.
           </p>
-        </div>
+        </div>-->
 
-        <img id="covid" class="covid__image img-fluid pb-3" src="<?php echo get_stylesheet_directory_uri(); ?>/images/covid.jpg" alt="Special COVID rules image" aria-label="COVID rules image" />
-          
+        <img id="covid" class="covid__image img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/images/covid.jpg" alt="Special COVID rules image" aria-label="COVID rules image" />
+        
+        <div class="faq__container">
+          <h2 class="faq__question">
+          At Grim Trails, we put enormous amounts of time and energy into building a quality show that is both safe and entertaining for guests. We want you to be scared, but for all the right reasons. That’s why we’re adding and enforcing all of the following measures to keep you safe: 
+          </h2>
+          <p class="faq__answer">- All guests, staff and monsters will be required to wear a face covering or mask in accordance with state mandates.</p>
+          <p class="faq__answer">- All monsters, staff and guests will be temperature checked before entering the attraction.  </p>
+          <p class="faq__answer">- Guests will explore the trail with only their household group to accompany them.</p>
+          <p class="faq__answer">- More time will be provided between guest groups to ensure adequate social distancing is maintained within the attraction. As normal, patrons will be instructed to stick together and keep going to avoid bumping into other groups. </p>
+          <p class="faq__answer">- We’ve expanded our queue line, adding plenty of room for maintaining six feet of social distance between your group and others. We’ve marked out every 10 ft of our queue line to help guests stay safe.</p>
+          <p class="faq__answer">- Please make use of our hand sanitizing stations when entering and exiting the attraction! </p>
+          <p class="faq__answer">- Routine cleaning both in individual scenes by the monsters that live there and throughout the attraction. We’ve reassigned staff who would regularly move throughout our queue line to focus on sanitizing the trail.</p>
+          <p class="faq__answer">- Additionally, we removed and eliminated many touch-points throughout our haunted house. We also installed plexiglass sneeze guards on our ticket booth and gift shop to protect both staff and patrons.  </p>
+          <p class="faq__answer">- All staff and monsters will also be screened for COVID nightly. </p>
+          <p class="faq__answer">- All monsters will have masks or face-coverings on. Monster masks which previously exposed the mouth and nose have been modified to cover both the nose and mouth.</p>
+          <p class="faq__answer">- Monsters will be assigned to one role and one costume for the entire season, to eliminate all sharing of masks and costumes. </p>
+          <p class="faq__answer">- We’ve also upped cleaning and expanded our makeup area to keep our monsters even safer. </p>
+          </div>
+
 </div>
 
 <?php get_footer(); ?>
